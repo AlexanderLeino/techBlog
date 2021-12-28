2490c1e8d0f7eedda4fa2a0af99aeab1ebd7f6e7
Alexander Leino <Alexander.Leino@yahoo.com>
2021-12-28 18:45:14 -0500

    added readme

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+      
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
